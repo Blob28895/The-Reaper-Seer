f4fee5fd34a6e167627cd63362a2ebc4877e9f5f
Blob28895 <blob28895@gmail.com>
2022-10-20 20:55:40 -0500

    made combat changes, more consistent comboing

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -62,28 +62,45 @@ public class PlayerCombat : MonoBehaviour
 		
 		string state = animator.GetCurrentAnimatorClipInfo(0)[0].clip.name;
 		//Debug.Log(state);
-		transition1Time = comboTransitionDelay;
-		transition2Time = secondTransitionDelay;
+		/*transition1Time = comboTransitionDelay;
+		transition2Time = secondTransitionDelay;*/
 		if (Time.time >= nextAttackTime)
 		{
 			if (Input.GetButtonDown("Fire3")) // if we push the fire button
 			{
+
+				/*This is a Multiline comment to kind of explain this code to both myself, and whomever may come across this mess
+				 
+				Next Attack Time: A time in the form of how much time has passed since the scene started, we only even check whether or not the
+				attack button is pressed if we are at or past the next Attack Time
+
+				State: just the name of the animation that is currently playing
+
+				comboTime: when this timer runs out it means the combo is over
+
+				inCombo: we only want to end the combo when we are actually in a combo to begin with, so thats why this is here
+
+				Attack(): the function handles many things, it decides which animation needs to play, plays it, then applies damage to things
+
+				The first if statement is just when we arent attacking, to be honest I could probably replace all the state checks and just check if we are
+				inCombo, but this seems to work
+
+				The else if statement is the check to see if we are doing the third attack this is checked before the 2nd attack check because i needed
+				the second attack check to be an else statement, so it needed to be at the end
+
+				The else statement is for the second attack check. To be honest, at the time of writing this I dont fully understand why I need this to be an else
+				statement and not an if else like for the second attack, but I tried this and it seems to be working
+				 */
 				if (state != "attack1" && state != "attack2" && state != "attack3" && state != "transition1" && state != "transition2")
 				{ //if we arent alredy attacking
 					Attack();
 					nextAttackTime = Time.time + attack1Time;
 					inCombo = true;
-					comboTime = Time.time + attack1Time + transition1Time;
-					//Debug.Log("Attack 1");
-				}
-				else if (state == "transition1" || state == "attack1")
-				{
-					Attack();
-					nextAttackTime = Time.time + attack2Time;
-					//Debug.Log(nextAttackTime);
-					inCombo = true;
-					comboTime = Time.time + attack2Time + transition2Time;
-					//Debug.Log("Attack 2");
+				//comboTime = Time.time + attack1Time + transition1Time;
+				comboTime = Time.time + attack1Time + comboTransitionDelay;
+				//Debug.Log("Attack 1");
+				//Debug.Log(attack1Time);
+					//Debug.Log(comboTime - nextAttackTime);
 				}
 				else if (state == "transtion2" || state == "attack2")
 				{
@@ -91,6 +108,19 @@ public class PlayerCombat : MonoBehaviour
 					nextAttackTime = Time.time + attack3Time + attackDelay;
 					inCombo = false;
 					//Debug.Log("Attack 3");
+				}
+				else /*if (state == "transition1"*/ /*|| state == "attack1")*/
+				{
+					Attack();
+					nextAttackTime = Time.time + attack2Time;
+					//Debug.Log(nextAttackTime);
+					inCombo = true;
+					//comboTime = Time.time + attack2Time + transition2Time;
+					comboTime = Time.time + attack2Time + comboTransitionDelay;
+					//Debug.Log("Attack 2");
+					//Debug.Log(attack2Time);
+					//Debug.Log(comboTime - nextAttackTime);
+				
 				}
 			}
 		}
