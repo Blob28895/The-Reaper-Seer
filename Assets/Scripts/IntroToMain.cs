94ae869b9efba49b926e7fa2c95da7249abfa0a8
Alejandro Moreno <alex.moreno161100@gmail.com>
2022-10-26 02:09:20 -0500

    Diologue Scene

---
+++ Assets/Scripts/IntroToMain.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroToMain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+ 
+}
