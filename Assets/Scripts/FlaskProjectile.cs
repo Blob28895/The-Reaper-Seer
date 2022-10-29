83540b2ffa3db797f130169994017d743fcbe8e8
Blob28895 <blob28895@gmail.com>
2022-10-29 16:23:12 -0500

    copied all changes from scientist animation branch and moved reaper health bar slightly

--- Assets/Scripts/FlaskProjectile.cs
+++ Assets/Scripts/FlaskProjectile.cs
@@ -5,30 +5,51 @@ using UnityEngine;
 public class FlaskProjectile : MonoBehaviour
 {
     private Vector3 targetPosition;
+    public int attackDamage = 1;
     public float speed;
-    public int spinSpeed = 90;
-    public Rigidbody2D rb;
+    public int spinSpeed = 135;
+    private Rigidbody2D rb;
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         Vector3 reaperPosition = FindObjectOfType<CharacterController2D>().transform.position;
         targetPosition.Set(reaperPosition.x, reaperPosition.y + 8f, 0);
         rb.AddForce((targetPosition - transform.position) * speed);
-        //targetPosition = reaperPosition;
+        // Change the spin direction depending on which direction the flask is thrown
+        if (targetPosition.x - transform.position.x < 0)
+        {
+            spinSpeed *= -1;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(new Vector3(0, 0, spinSpeed * Time.deltaTime));
-        //transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
     }
+
+    // Function that runs when the flask collides with something
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.name != "Scientist" && collision.name != "Platform" && collision.gameObject.tag != "Vent")
+        Debug.Log(collision.name);
+        // Do not collide with the scientist, gas, or the platform
+        if (collision.name != "Scientist" && enabled && collision.name != "Platform" && collision.gameObject.tag != "Vent")
         {
             //Debug.Log(collision.name);
-            Destroy(gameObject);
+            enabled = false;
+            // If the flask hits the Reaper, deal damage
+            if (collision.name == "Reaper")
+            {
+                collision.GetComponent<Player>().TakeDamage(attackDamage);
+            }
+            // Remove the flask object after colliding
+            rb.constraints = RigidbodyConstraints2D.FreezeAll;
+            animator.SetTrigger("Break");
+            // float time = animator.GetCurrentAnimatorClipInfo(0).Length;
+            Destroy(gameObject, 0.75f);
         }
     }
 }
