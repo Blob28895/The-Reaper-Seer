17a64bd306b4cf4ab238f62e939412d77e48eae7
danny <danielolvera311@gmail.com>
2022-10-31 15:27:23 -0500

    Now fades properly between level 1 and scientist fight

--- Assets/Scripts/ElevatorButton.cs
+++ Assets/Scripts/ElevatorButton.cs
@@ -6,22 +6,32 @@ public class ElevatorButton : MonoBehaviour
 {
 	private bool transition = false;
 	public GameObject Keycard;
+	public Animator fadeAnimator;
+	public float transitionTime = 1f;
 
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
 		if (transition && collision.name == "Reaper")
 		{
-			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+			//SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 			transition = false;
+			StartCoroutine(TransitionScene());
 		}
 	}
 	private void OnTriggerStay2D(Collider2D collision)
 	{
 		if (transition && collision.name == "Reaper")
 		{
-			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+			//SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 			transition = false;
+			StartCoroutine(TransitionScene());
 		}
+	}
+	IEnumerator TransitionScene()
+    {
+		fadeAnimator.SetTrigger("Start");
+		yield return new WaitForSeconds(transitionTime);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 	}
 	// Update is called once per frame
 	void Update()
