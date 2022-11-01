9071de864bf41419a30be77ce50464b20ebee041
Daniel <danielolvera311@gmail.com>
2022-11-01 14:08:15 -0500

    Added win screen after defeating the scientist

--- Assets/Scripts/WinCondition.cs
+++ Assets/Scripts/WinCondition.cs
@@ -5,20 +5,28 @@ using UnityEngine;
 public class WinCondition : MonoBehaviour
 {
     public Transform target;
+    private bool show = false;
 
     // Update is called once per frame
     void Update()
     {
         //Debug.Log(target.position.x);
         // Checks to see if the Reaper is near the end of the level
-        if (target.position.x >= 132)
+        if (!show && !GameObject.FindGameObjectWithTag("Boss").GetComponent<Boss>().enabled)
         {
-            // Moves the win screen to the Reaper's location
-            transform.position = target.position;
-            // Enables the Sprite renderer to have the win screen appear
-            GetComponent<SpriteRenderer>().enabled = true;
-            // Freezes the Reaper in place
-            FindObjectOfType<Player>().GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+            show = true;
+            Debug.Log("Win Triggered");
+            StartCoroutine(WinScreen());
         }
+    }
+    IEnumerator WinScreen()
+    {
+        yield return new WaitForSeconds(3f);
+        // Moves the win screen to the Reaper's location
+        //transform.position = target.position;
+        // Enables the Sprite renderer to have the win screen appear
+        GetComponent<SpriteRenderer>().enabled = true;
+        // Freezes the Reaper in place
+        // FindObjectOfType<Player>().GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
     }
 }
