7a285911784e64bb1ae0208b60cdf18fa2d2a7cb
danny <danielolvera311@gmail.com>
2022-11-04 14:39:49 -0500

    Buttons in main menu are only selected if using a controller/keyboard

---
+++ Assets/Scripts/GamepadKeyboardNavigation.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GamepadKeyboardNavigation : MonoBehaviour
+{
+    public GameObject firstSelected;
+    private bool usingControllerOrKeyboard = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("Starting Menu Controller Script");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0 || Input.GetButtonDown("Submit")) && !usingControllerOrKeyboard)
+        {
+            usingControllerOrKeyboard = true;
+            EventSystem.current.SetSelectedGameObject(firstSelected);
+        }
+        else if ((Input.GetAxisRaw("Mouse X") != 0 || Input.GetAxisRaw("Mouse Y") != 0) && usingControllerOrKeyboard)
+        {
+            usingControllerOrKeyboard = false;
+            EventSystem.current.SetSelectedGameObject(null);
+        }
+    }
+}
