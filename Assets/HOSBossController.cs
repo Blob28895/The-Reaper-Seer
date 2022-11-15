e1b49c7265422ba334328126288b324cc840bac0
Daniel <danielolvera311@gmail.com>
2022-11-15 15:07:50 -0600

    Head guard can now move, attempted to fix hos head size in healthbar

--- Assets/HOSBossController.cs
+++ Assets/HOSBossController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class HOSBossController : MonoBehaviour
 {
     public float speed;
+    // Following are used for melee attacking
     public int attackDamage = 2;
     public float attackRange = 0.5f;
     public Transform attackPoint;
@@ -12,9 +13,24 @@ public class HOSBossController : MonoBehaviour
     public LayerMask playerLayer;
     private List<string> meleeAnimations = new List<string>();
     private float nextAttackTime = 0f;
+
+    // Smash attacks
+    public float smashAttackRate = 0.1f;
+    private float nextSmashTime = 0f;
+
+    // Call reinforcements
+    public float callRate = 0.1f;
+    private float nextCallRate = 0f;
+
+    // Reaper stuff
+    public Transform target;
+    private float dist;
+
+    // Used for head guard
     private bool facingRight = false;
     private Animator animator;
     private int maxHealth;
+    private int health;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,9 +46,25 @@ public class HOSBossController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Head guard will start attacking once the Reaper is in range
-        if (PlayerInRange())
+        health = GetComponent<Boss>().getCurrentHealth();
+        dist = target.position.x - transform.position.x;
+        if (dist > 0 && !facingRight)
         {
+            Flip();
+        }
+        else if (dist < 0 && facingRight)
+        {
+            Flip();
+        }
+        if (!PlayerInRange())
+        {
+            animator.SetBool("Moving", true);
+            MoveTowards();
+        }
+        // Head guard will start attacking once the Reaper is in range
+        else if (PlayerInRange())
+        {
+            animator.SetBool("Moving", false);
             if (Time.time >= nextAttackTime)
             {
                 Debug.Log("Attacking!");
@@ -44,11 +76,23 @@ public class HOSBossController : MonoBehaviour
         }
     }
 
+    // Check if the Head guard can move
+    private bool CanMove()
+    {
+        return false;
+    }
+
     // Detects whether the Reaper is in range or not
     private bool PlayerInRange()
     {
         Collider2D hitPlayer = Physics2D.OverlapCircle(attackPoint.position, attackRange, playerLayer);
         return hitPlayer != null;
+    }
+
+    // Move towards the Reaper
+    private void MoveTowards()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(target.position.x, transform.position.y), speed * Time.deltaTime);
     }
 
     // Deal damage to the Reaper if the attack hits him
