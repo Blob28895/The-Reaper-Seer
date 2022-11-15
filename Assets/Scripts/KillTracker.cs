68be8a36a1a8bdf8e39889ddb0108d4c21759750
Blob28895 <blob28895@gmail.com>
2022-11-14 18:26:46 -0600

    added souls healing system, ID card drops from a random enemy now

--- Assets/Scripts/KillTracker.cs
+++ Assets/Scripts/KillTracker.cs
@@ -6,16 +6,36 @@ public class KillTracker : MonoBehaviour
 {
     List<GameObject> listOfEnemys = new List<GameObject>();
     public GameObject Keycard;
+    public SoulBar souls;
+    private Player player;
     // Start is called before the first frame update
     void Start()
     {
         listOfEnemys.AddRange(GameObject.FindGameObjectsWithTag("Enemy"));
+        souls.SetMax(staticVariables.maxSouls);
+        souls.SetCurrent(staticVariables.currSouls);
+        player = GetComponent<Player>();
     }
     public void KilledEnemy(GameObject Enemy)
     {
         if (listOfEnemys.Contains(Enemy))
         {
+            if (staticVariables.currSouls < staticVariables.maxSouls)
+            {
+                staticVariables.currSouls += 1;
+            }
+            if(staticVariables.currSouls >= staticVariables.maxSouls){
+                staticVariables.currSouls -= staticVariables.maxSouls;
+                player.Heal(1);
+                
+			}
+            souls.SetCurrent(staticVariables.currSouls);
             listOfEnemys.Remove(Enemy);
+            int chance = Random.Range(0, listOfEnemys.Count);
+            if(chance == 0)
+			{
+                Keycard.SetActive(true);
+			}
         }
         AreOpponentsDead();
         //print(listOfEnemys.Count);
